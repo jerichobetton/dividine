4501c4854e2800f9d66431ec3ec96ea07ae663f0
Jericho Betton <jerichobetton@gmail.com>
2024-01-31 17:59:25 -0500

    added public key credentials and added passport serialization

--- app/models/publickeycredentials.js
+++ app/models/publickeycredentials.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class PublicKeyCredentials extends Model {
     /**
@@ -11,13 +9,20 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.User, { foreignKey: "user_id" });
     }
   }
-  PublicKeyCredentials.init({
-    public_key: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'PublicKeyCredentials',
-  });
+  PublicKeyCredentials.init(
+    {
+      user_id: DataTypes.INTEGER,
+      external_id: DataTypes.STRING,
+      public_key: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "PublicKeyCredentials",
+      tableName: "public_key_credentials",
+    }
+  );
   return PublicKeyCredentials;
 };
