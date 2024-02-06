6b86737d6ca35e81176949b9912bfb048e6b8e5c
Jericho Betton <jerichobetton@gmail.com>
2024-02-05 21:47:18 -0500

    added update bill

--- backend/routes/billRoutes.js
+++ backend/routes/billRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
-
-const Bill = require("../models/bill");
-
 const router = express.Router();
+const Bill = require("../models/bill");
 
 router.post("/", async (req, res) => {
   try {
@@ -16,6 +14,22 @@ router.post("/", async (req, res) => {
 
     const savedBill = await newBill.save();
     res.json(savedBill);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const { totalAmount, numberOfPeople, tip } = req.body;
+    const updateResponse = await Bill.updateOne(
+      { totalAmount },
+      { numberOfPeople },
+      { tip }
+    );
+    updateResponse.acknowledged
+      ? res.json(updateResponse)
+      : res.status(400).json({ error: `unable to update bill information` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
