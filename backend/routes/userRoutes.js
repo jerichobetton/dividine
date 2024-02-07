b9a738199f5aec31ae4b5270ed34ede81d29ca19
Jericho Betton <jerichobetton@gmail.com>
2024-02-06 22:13:26 -0500

    added login route, get user, and password hashing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   try {
-    const { username, email } = req.body;
+    const { username, email, password } = req.body;
 
+    // Check if username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user **ADD PASSWORD AND OTHER PROPERTIES**
     const newUser = new User({
       username,
       email,
+      password: hashedPassword,
       // other user properties
     });
 
@@ -21,13 +34,55 @@ router.post("/", async (req, res) => {
 
 router.put("/", async (req, res) => {
   try {
-    const { username, ...rest } = req.body;
-    const updateResponse = await User.updateOne({ username }, { ...rest });
+    const { username, password } = req.body;
+    const updateResponse = await User.updateOne({ username }, { password });
     updateResponse.acknowledged
       ? res.json(updateResponse)
       : res.status(400).json({ error: `unable to update user ${username}` });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+// Get User
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(404).json({ message: "No users found" });
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login Api
+router.post("/", async (req, res) => {
+  // TODO: Implement user login logic
+
+  try {
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    // Compare password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Logged in successfully", userId: user._id });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
