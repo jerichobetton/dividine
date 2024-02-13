7f9d97899ae86be795643317102cb14dfe16a13e
Elias Roussos <eliasisaiah@outlook.com>
2024-02-12 21:37:06 -0500

    implement create new bill api call

--- dividine_project/src/App.jsx
+++ dividine_project/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Dash from './pages/Dash'; 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Hero from './components/Hero'; 
-import './app.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Dash from "./pages/Dash";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Hero from "./components/Hero";
+import "./app.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Header />
-        
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/dash" element={<Dash />} /> 
+          <Route path="/dash" element={<Dash />} />
         </Routes>
         <Footer />
       </div>
--- dividine_project/src/components/DashForm.jsx
+++ dividine_project/src/components/DashForm.jsx
@@ -1,43 +1,95 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './form.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./form.css";
 
 function DashForm() {
-    const [amount, setAmount] = useState('');
-    const [numPeople, setNumPeople] = useState('');
-    const [totalBill, setTotalBill] = useState('');
+  const [amount, setAmount] = useState("");
+  const [numPeople, setNumPeople] = useState("");
+  const [totalBill, setTotalBill] = useState("");
 
-    const handleTotal = (e) => {
-        e.preventDefault();
-        console.log('Calculating:', amount, numPeople, totalBill);
+  const handleTotal = (e) => {
+    e.preventDefault();
+    console.log("Calculating:", amount, numPeople, totalBill);
+  };
+
+  const handleRequest = (e) => {
+    e.preventDefault();
+    console.log("Requesting:", amount, numPeople, totalBill);
+  };
+
+  const userName = "elias";
+  const tip = 5.0;
+
+  const handleSend = async (e) => {
+    e.preventDefault();
+    const bill = {
+      userName,
+      totalAmount: amount,
+      numberOfPeople: numPeople,
+      tip,
     };
-    
-    const handleRequest = (e) => {
-        e.preventDefault();
-        console.log('Requesting:', amount, numPeople, totalBill);
-    };
+    console.log("Sending:", amount, numPeople, totalBill);
 
-    const handleSend = (e) => {
-        e.preventDefault();
-        console.log('Sending:', amount, numPeople, totalBill);
-    };
+    const data = await postData("/api/v1/bill", bill);
+    console.log(data);
+  };
 
-    return (
-        <form className='form'>
-            <label htmlFor="amount">Amount:</label>
-            <input type="text" id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+  // Example POST method implementation:
+  async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
 
-            <label htmlFor="numPeople">Number of People:</label>
-            <input type="text" id="numPeople" name="numPeople" value={numPeople} onChange={(e) => setNumPeople(e.target.value)} required />
+  return (
+    <form className="form">
+      <label htmlFor="amount">Amount:</label>
+      <input
+        type="text"
+        id="amount"
+        name="amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        required
+      />
 
-            <label htmlFor="totalBill">You All Owe $$$:</label>
-            <input type="text" id="totalBill" name="totalBill" value={totalBill} onChange={(e) => setTotalBill(e.target.value)} required />
+      <label htmlFor="numPeople">Number of People:</label>
+      <input
+        type="text"
+        id="numPeople"
+        name="numPeople"
+        value={numPeople}
+        onChange={(e) => setNumPeople(e.target.value)}
+        required
+      />
 
-            <button type="button" onClick={handleTotal}>Calculate Total</button>
-            <button type="button" onClick={handleRequest}>Request</button>
-            <button type="button" onClick={handleSend}>Send</button>
-        </form>
-    );
+      <label htmlFor="totalBill">You All Owe $$$:</label>
+      <input
+        type="text"
+        id="totalBill"
+        name="totalBill"
+        value={totalBill}
+        onChange={(e) => setTotalBill(e.target.value)}
+        required
+      />
+
+      <button type="button" onClick={handleTotal}>
+        Calculate Total
+      </button>
+      <button type="button" onClick={handleRequest}>
+        Request
+      </button>
+      <button type="button" onClick={handleSend}>
+        Send
+      </button>
+    </form>
+  );
 }
 
 export default DashForm;
