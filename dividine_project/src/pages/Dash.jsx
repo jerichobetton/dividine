37d324b102282043cb05eca2ee5de9769974e0a1
michael <marbm23@gmail.com>
2024-02-16 13:28:47 -0800

    updated images

--- dividine_project/src/pages/Dash.jsx
+++ dividine_project/src/pages/Dash.jsx
@@ -4,7 +4,11 @@ import DashForm from '../components/DashForm';
 function Dash() {
   return (
     <div className="app-content">
-      
+      <div className="avatar">
+  <div className="w-24">
+    <img src="https://plus.unsplash.com/premium_photo-1661478265448-1dc9b0eaf5d3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHJlc3RhdXJhbnQlMjBiaWxsfGVufDB8fDB8fHww" alt="check" className="check"/>
+  </div>
+</div>
       <h2>Let us DiviDine your bill!</h2>
       <DashForm /> 
     </div>
